359682fce37dbbca56dfa1ac40b611651902e6c5
Jack Park <jackpark.dev@gmail.com>
2022-02-27 17:29:37 +0900

    Apply react 18.0.0

--- src/App.js
+++ src/App.js
@@ -1,38 +1,26 @@
-import { useEffect, useState } from 'react';
-import { unstable_batchedUpdates } from 'react-dom';
+import { useMemo, useState } from 'react';
 import './App.css';
 
 const getUserData = () => fetch('/user.json').then((resp) => resp.json());
 
 const App = () => {
-  const [name, setName] = useState();
-  const [roles, setRoles] = useState();
-  const [roleList, setRoleList] = useState();
-  useEffect(() => {
-    console.log(`userEffect ${name} ${roles}`);
-    if (name) {
-      setRoleList(Object.keys(roles ?? {}).filter((k) => roles[k]));
-    }
-  }, [name, roles]);
+  const [info, setInfo] = useState({
+    name: '',
+    roles: [],
+  });
+  const roleList = useMemo(() => {
+    return Object.keys(info.roles ?? {}).filter((k) => info.roles[k]);
+  }, [info.roles]);
 
-  /* const onLoadUser = () => {
-    setName("Test User");
-    setRoles({
-      editor: true,
-    });
-  }; */
   const onLoadUser = async () => {
     const data = await getUserData();
     console.log('TCL: onLoadUser -> data', data);
-    unstable_batchedUpdates(() => {
-      setName(data.name);
-      setRoles(data.roles);
-    });
+    setInfo(data);
   };
   return (
     <div className="App">
-      <div>Name: {JSON.stringify(name)}</div>
-      <div>Roles: {JSON.stringify(roles)}</div>
+      <div>Name: {JSON.stringify(info.name)}</div>
+      <div>Roles: {JSON.stringify(info.roles)}</div>
       <div>Role List: {JSON.stringify(roleList)}</div>
       <div>
         <button onClick={onLoadUser}>Load</button>
